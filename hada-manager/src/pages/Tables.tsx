6ddf5ecbcc2d46183389cea7e9a140e37eb4963f
Omrron <obaron4120@gmail.com>
2025-06-21 01:28:57 +0300

    Made tables from array

--- hada-manager/src/pages/Tables.tsx
+++ hada-manager/src/pages/Tables.tsx
@@ -1,13 +1,18 @@
 import { Table } from "../components/Table";
+import type { TableType } from "../Types";
+
+const tables : TableType[] = [
+    {name:"חד\"א קצינים וואי יש לו שם ממש ארוך", occupancy:5, capacity:10, reserved: false},
+    {name:"חד\"א שומרים", occupancy:5, capacity:10, reserved: false},
+    {name:"חד\"א סדיר", occupancy:5, capacity:10, reserved: false},
+    {name:"חד\"א לאלרגיות מוזרות", occupancy:5, capacity:10, reserved: false},
+];
 
 export function Tables() {
+
     return (
         <div className="flex-container">
-           <Table name="חד&quot;א קצינים" current={2} capacity={5}/>
-           <Table name="חד&quot;א קצינים" current={2} capacity={5}/>
-           <Table name="חד&quot;א קצינים" current={2} capacity={5}/>
-           <Table name="חד&quot;א קצינים" current={2} capacity={5}/>
-           <Table name="חד&quot;א קצינים" current={2} capacity={5}/>
+            {tables.map(table => <Table name={table.name} current={table.occupancy} capacity={table.capacity}/>)}
         </div>
     )
 }
