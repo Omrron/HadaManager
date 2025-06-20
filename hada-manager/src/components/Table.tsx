85c70267facdf9d239383801496347f5d3902d41
Omrron <obaron4120@gmail.com>
2025-06-21 01:29:15 +0300

    added reserved checkbox
    currently does nothing

--- hada-manager/src/components/Table.tsx
+++ hada-manager/src/components/Table.tsx
@@ -10,7 +10,7 @@ export function Table({name, current, capacity} : Props)
 
     return (
     <div className="component-container room-item-container">
-      <h2>
+      <h2 className="short-text">
         {name}
       </h2>
       <div>
@@ -18,6 +18,10 @@ export function Table({name, current, capacity} : Props)
         <span>
           {current} / {capacity}
         </span>
+      </div>
+      <div>
+        <span>תפוס</span>
+        <input type="checkbox"/>
       </div>
     </div>
   );
