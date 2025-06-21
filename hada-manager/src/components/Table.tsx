a8751808408dd3d7b3763ee495db59ef1c083a32
Omrron <obaron4120@gmail.com>
2025-06-21 15:55:02 +0300

    pretty meter bars

--- hada-manager/src/components/Table.tsx
+++ hada-manager/src/components/Table.tsx
@@ -7,17 +7,21 @@ interface Props{
 export function Table({name, current, capacity} : Props)
 {
     const isFull = current >= capacity;
-
+    const isNearFull = current/capacity >= 0.8;
     return (
     <div className="component-container room-item-container">
       <h2 className="short-text">
         {name}
       </h2>
       <div>
-        <span>People </span>
+        <span>נוכחים </span>
         <span>
           {current} / {capacity}
         </span>
+        <div className="progress-container">
+          <div className={`progress ${isFull ? 'bad' : isNearFull ? 'ok' : 'good'}`}
+               style={{width:`${(current/capacity)*100}%`}}/>
+        </div>
       </div>
       <div>
         <span>תפוס</span>
--- hada-manager/src/pages/Tables.tsx
+++ hada-manager/src/pages/Tables.tsx
@@ -3,9 +3,9 @@ import type { TableType } from "../Types";
 
 const tables : TableType[] = [
     {name:"חד\"א קצינים וואי יש לו שם ממש ארוך", occupancy:5, capacity:10, reserved: false},
-    {name:"חד\"א שומרים", occupancy:5, capacity:10, reserved: false},
-    {name:"חד\"א סדיר", occupancy:5, capacity:10, reserved: false},
-    {name:"חד\"א לאלרגיות מוזרות", occupancy:5, capacity:10, reserved: false},
+    {name:"חד\"א שומרים", occupancy:10, capacity:10, reserved: false},
+    {name:"חד\"א סדיר", occupancy:9, capacity:10, reserved: false},
+    {name:"חד\"א לאלרגיות מוזרות", occupancy:100, capacity:100, reserved: false},
 ];
 
 export function Tables() {
