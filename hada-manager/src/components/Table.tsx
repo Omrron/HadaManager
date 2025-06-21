9324e46e99803f09f4c868d5673c461b03345eef
Omrron <obaron4120@gmail.com>
2025-06-21 17:23:22 +0300

    I think I finished the rooms

--- hada-manager/src/components/Table.tsx
+++ hada-manager/src/components/Table.tsx
@@ -1,32 +1,37 @@
-interface Props{
-    name : string
-    current : number
-    capacity : number
+import { ShortenText } from "../common/textFunctions";
+
+interface Props {
+  name: string;
+  current: number;
+  capacity: number;
 }
 
-export function Table({name, current, capacity} : Props)
-{
-    const isFull = current >= capacity;
-    const isNearFull = current/capacity >= 0.8;
-    return (
+export function Table({ name, current, capacity }: Props) {
+  const isFull = current >= capacity;
+  const isNearFull = current / capacity >= 0.8;
+  const shortText = ShortenText(name, 11);
+
+  return (
     <div className="component-container room-item-container">
-      <h2 className="short-text">
-        {name}
-      </h2>
-      <div>
+      <h2 className="center">{shortText}</h2>
+      <div className="center">
         <span>נוכחים </span>
-        <span>
+        <span dir="ltr">
           {current} / {capacity}
         </span>
         <div className="progress-container">
-          <div className={`progress ${isFull ? 'bad' : isNearFull ? 'ok' : 'good'}`}
-               style={{width:`${(current/capacity)*100}%`}}/>
+          <div
+            className={`progress ${
+              isFull ? "bad" : isNearFull ? "ok" : "good"
+            }`}
+            style={{ width: `${(current / capacity) * 100}%` }}
+          />
         </div>
       </div>
-      <div>
+      <div className="center">
+        <input type="checkbox" />
         <span>תפוס</span>
-        <input type="checkbox"/>
       </div>
     </div>
   );
-};
+}
