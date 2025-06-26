04138dc76438a9dc75a138c28f77d1cea0ac62f1
Omrron <obaron4120@gmail.com>
2025-06-26 11:37:24 +0300

    Made room component

---
+++ hada-manager/src/components/Room.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { ShortenText } from "../common/textFunctions";
+import { Link } from "react-router-dom";
+
+interface Props {
+  id:string;
+  name: string;
+  occupancy: number;
+  capacity: number;
+}
+
+export function Room({id, name, occupancy, capacity}: Props) {
+  const isFull = occupancy >= capacity;
+  const isNearFull = occupancy / capacity >= 0.8;
+  const shortText = ShortenText(name, 11);
+
+  return (
+    <Link key={id} to={`/tables/${id}`}>
+      <button className="component-container">
+        <h2 className="center">{shortText}</h2>
+        <div className="center">
+          <span>סועדים </span>
+          <span dir="ltr">
+            {occupancy} / {capacity}
+          </span>
+          <div className="progress-container">
+            <div
+              className={`progress ${
+                isFull ? "bad" : isNearFull ? "ok" : "good"
+              }`}
+              style={{ width: `${(occupancy / capacity) * 100}%` }}
+            />
+          </div>
+        </div>
+      </button>
+    </Link>
+  );
+}
