4d33f8234e413fcd23b5156f9f7dc749a2656968
Omrron <obaron4120@gmail.com>
2025-06-27 13:18:10 +0300

    Fitted layout and created a container component

---
+++ hada-manager/src/components/PeopleList.tsx
@@ -0,0 +1,5 @@
+export function PeopleList(){
+    return(
+        <div></div>
+    );
+}
--- hada-manager/src/layouts/PageLayout.tsx
+++ hada-manager/src/layouts/PageLayout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { PeopleList } from "../components/PeopleList";
 
 export function Layout(){
     return(
-        <div className="list-container">
-            <aside>
+        <div className="layout-container">
+            <aside className="list-container">
                 <PeopleList/>
             </aside>
-            <div className="layout-container">
-                <Outlet/>
+            <div style={{width:"80vw"}}>
+                <div style={{height:"10vh", backgroundColor:"green", display:"flex"}}>
+
+                </div>
+                <div className="content-container">
+                    <Outlet/>
+                </div>
             </div>
         </div>
     )
