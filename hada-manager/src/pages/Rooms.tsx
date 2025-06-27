e2bd274813b0f7345545cbd67acdf1c6e578109c
Omrron <obaron4120@gmail.com>
2025-06-27 13:17:39 +0300

    fixed some logic styling errors

--- hada-manager/src/pages/Rooms.tsx
+++ hada-manager/src/pages/Rooms.tsx
@@ -3,14 +3,15 @@ import type { RoomType } from "../Types"
 
 const roomsList : RoomType[] = [{id:"123456", capacity:50, name:"חד\"א קצינים שיש לו עכשיו שם ממש אבל ממש אבל מממממששששששש ארוך",
                                     tables:[{name:"שולחן בנים", capacity:50, peopleIds:[], id:"123456"}],
-                                    people:[{id:867445, name:"omar", tableId:"123456"}]}];
+                                    people:[{id:867445, name:"omar", tableId:"123456"}]},
+                                {id:"123456", capacity:8, name:"חד\"א חפשים",
+                                    tables:[{name:"שולחן בנים", capacity:8, peopleIds:[867445], id:"123456"}],
+                                    people:[{id:867445, name:"omar", tableId:"123456"}]},];
 
 export function Rooms() {
     return (
         <>
-            <div>
-                {roomsList.map(_ => <Room key={_.id} id={_.id} name={_.name} capacity={_.capacity} people={_.people} tables={_.tables}/>)}
-            </div>
+            {roomsList.map(_ => <Room key={_.id} id={_.id} name={_.name} capacity={_.capacity} people={_.people} tables={_.tables}/>)}
         </>
     )
 }
