d880e328d80099a0ef5155867099eadcf80f96f6
Omrron <obaron4120@gmail.com>
2025-06-28 15:31:17 +0300

    table long name tooltip

--- hada-manager/src/components/Table.tsx
+++ hada-manager/src/components/Table.tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { ShortenText } from "../common/textFunctions";
 import type { TableType } from "../Types";
 
-export function Table({ name, peopleIds, capacity, reserved=false }: TableType) {
+export function Table({
+  name,
+  peopleIds,
+  capacity,
+  reserved = false,
+}: TableType) {
   var occupancy = peopleIds.length;
   const isFull = occupancy >= capacity;
   const isNearFull = occupancy / capacity >= 0.8;
@@ -11,7 +16,9 @@ export function Table({ name, peopleIds, capacity, reserved=false }: TableType) 
 
   return (
     <div className="component-container link-component-container">
-      <h2 className="center"><abbr title={`${shortText!=name ? name : ""}`}>{shortText}</abbr></h2>
+      <h2 className="center">
+        <abbr title={`${shortText != name ? name : ""}`}>{shortText}</abbr>
+      </h2>
       <div className="center">
         <span>סועדים </span>
         <span dir="ltr">
@@ -27,7 +34,7 @@ export function Table({ name, peopleIds, capacity, reserved=false }: TableType) 
         </div>
       </div>
       <div className="center">
-        <input type="checkbox" onClick={() => setReserved(!isReserved)}/>
+        <input type="checkbox" onClick={() => setReserved(!isReserved)} />
         <span>תפוס</span>
       </div>
     </div>
