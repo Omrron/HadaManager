4e3c9f77f8bcfb5e2ea1fad1b4d658de2a6dadcd
Omrron <obaron4120@gmail.com>
2025-07-19 11:35:55 +0300

    formatting all the way

--- hada-manager/src/Types.ts
+++ hada-manager/src/Types.ts
@@ -1,33 +1,33 @@
-export interface RoomType {
-    id: string
-    name: string,
+export type RoomType = {
+  id: string
+  name: string
 }
 
-export interface TableType {
-    peopleIds: string[],
-    roomId: string,
-    id: string,
-    name: string,
-    capacity: number,
-    reserved?: boolean
+export type TableType = {
+  peopleIds: string[]
+  roomId: string
+  id: string
+  name: string
+  capacity: number
+  reserved?: boolean
 }
 
-export interface PersonType {
-    roomName?: string,
-    tableName?: string,
-    id: string
-    name: string,
-    eatingState: number,
-    idNumber: number,
+export type PersonType = {
+  roomName?: string
+  tableName?: string
+  id: string
+  name: string
+  eatingState: number
+  idNumber: number
 }
 
-export interface TopLevelProps {
-    editMode: boolean;
-    setEditMode: React.Dispatch<React.SetStateAction<boolean>>;
-    people: PersonType[];
-    setPeople: React.Dispatch<React.SetStateAction<PersonType[]>>;
-    tables: TableType[];
-    setTables: React.Dispatch<React.SetStateAction<TableType[]>>;
-    rooms: RoomType[];
-    setRooms: React.Dispatch<React.SetStateAction<RoomType[]>>;
+export type TopLevelProps = {
+  editMode: boolean
+  setEditMode: React.Dispatch<React.SetStateAction<boolean>>
+  people: PersonType[]
+  setPeople: React.Dispatch<React.SetStateAction<PersonType[]>>
+  tables: TableType[]
+  setTables: React.Dispatch<React.SetStateAction<TableType[]>>
+  rooms: RoomType[]
+  setRooms: React.Dispatch<React.SetStateAction<RoomType[]>>
 }
